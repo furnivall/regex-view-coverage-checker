a6e5bf07f972d18b553cea4091d6f4039d09ceed
Daniel Furnivall <danielfurnivall@gmail.com>
2022-11-08 17:46:17 +0000

    refactored to be smaller

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ import os
 import re
 my_dir = os.path.expanduser("~")
 
-list_of_mains = os.listdir('../../Work/pip-frontend/src/main/views')
-list_of_tests = os.listdir('../../Work/pip-frontend/src/test/unit/views')
-
-list_of_stripped_tests = [re.sub(r"['$&+,:;=?@#|'<>.^*()%!-]", "", i.split('.')[0]).lower() for i in list_of_tests]
-
-
-output = {}
-
-list_of_stripped_mains = [re.sub(r"[$&+,:;=?@#|'<>.^*()%!-]", "", i.split('.')[0]).lower() for i in list_of_mains]
+list_of_stripped_tests = [re.sub(r"['$&+,:;=?@#|<>.^*()%!-]", "", i.split('.')[0]).lower() for i in os.listdir('../../Work/pip-frontend/src/main/views')]
+list_of_stripped_mains = [re.sub(r"[$&+,:;=?@#|'<>.^*()%!-]", "", i.split('.')[0]).lower() for i in os.listdir('../../Work/pip-frontend/src/test/unit/views')]
 
 x = {i : i in list_of_stripped_tests for i in list_of_stripped_mains}
 
 for k, v in x.items():
     print(k, v)
-
